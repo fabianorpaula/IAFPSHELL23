c50c17f652f6054994058b14399b72f075da30d3
Fabiano De Paula <fabiano.rpaula@gmail.com>
2023-03-16 16:08:45 -0300

    Soldado2

--- Assets/Scripts/Soldado.cs
+++ Assets/Scripts/Soldado.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Soldado : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private NavMeshAgent Agente;
+    public List<GameObject> Caminhos;
+    private int ponteiro;
     void Start()
     {
-        
+        Agente = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
